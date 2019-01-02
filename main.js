40e7b4434af2f03a6f697e07c363643ebb8cb759
Ubuntu <ubuntu@ip-172-31-24-171.us-east-2.compute.internal>
2019-01-02 18:12:12 +0000

    test

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from UpdatedLambda,DIFIED..!'),
+        body: JSON.stringify('Hello from UpdatedLambda,DIFIED. testing.!'),
     };
     return response;
 };
