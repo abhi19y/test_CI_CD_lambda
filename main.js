36cde56c493bcaffa4036eaa1eb0b60f4a9e834a
Ubuntu <ubuntu@ip-172-31-24-171.us-east-2.compute.internal>
2019-01-03 15:22:42 +0000

    commit after webhook req working on github

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from UpdatedLambda,DIFIED. testing.!'),
+        body: JSON.stringify('Hello CHECK'),
     };
     return response;
 };
