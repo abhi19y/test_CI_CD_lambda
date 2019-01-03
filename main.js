58acaefd98a4b1532188dd96c602eecafcd574f6
Ubuntu <ubuntu@ip-172-31-24-171.us-east-2.compute.internal>
2019-01-03 16:02:11 +0000

    webhook

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello CHECK'),
+        body: JSON.stringify('Hello CHECK WEBHOOK'),
     };
     return response;
 };
