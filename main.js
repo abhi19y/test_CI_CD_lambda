8cdb52ca229dc86601f432fdce3067dd7a9593c9
Ubuntu <ubuntu@ip-172-31-24-171.us-east-2.compute.internal>
2019-01-03 20:17:49 +0000

    final test

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello NodeJS App , added aws cli cmd on jenkins TESTING LAMBDA JENKINS WEBHOOK'),
+        body: JSON.stringify('FINAL TESTING-> LAMBDA with AWS CLI'),
     };
     return response;
 };
