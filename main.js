df42bf9bd4061184b547e9930f96fa9e8e7316fe
Ubuntu <ubuntu@ip-172-31-24-171.us-east-2.compute.internal>
2019-01-03 19:08:16 +0000

    checking lambda funcy

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello CHECK WEBHOOK come on'),
+        body: JSON.stringify('Hello WORLD-> TESTING LAMBDA JENKINS WEBHOOK'),
     };
     return response;
 };
