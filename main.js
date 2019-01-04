89eecb765c8731cc5a25a59cee2aee38f9584a95
Ubuntu <ubuntu@ip-172-31-24-171.us-east-2.compute.internal>
2019-01-04 03:48:12 +0000

    Updation

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('FINAL TESTING-> LAMBDA with AWS CLI'),
+        body: JSON.stringify('FINAL TESTING -----> LAMBDA with AWS CLI'),
     };
     return response;
 };
